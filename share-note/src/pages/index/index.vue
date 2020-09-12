3616a561afb503f810633272a13ee32b44a5056b
badly_j <zhangjun@xports.cn>
2020-09-12 23:29:56 +0800

    初始化

---
+++ share-note/src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="index">
+    <h1>One Day</h1>
+  </div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style >
+  .index{
+    color: red;
+  }
+</style>
